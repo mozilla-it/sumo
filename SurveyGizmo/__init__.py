9c8e0e7cb07077793641a5f45c2b516d23886add
ophie200 <ophie200@users.noreply.github.com>
2019-02-02 00:53:35 -0800

    Update __init__.py

--- SurveyGizmo/__init__.py
+++ SurveyGizmo/__init__.py
@@ -1,6 +1,7 @@
 import json
 import requests
 import base64
+import logging
 
 def get_answer(survey_data_row, question_num, default):
     try:
@@ -19,7 +20,6 @@ def get_survey_data_row(row):
 
 def get_survey_data(api_url_base, params):
 	api_url = '{0}?_method=GET'.format(api_url_base)
-	
 	results = []
 	response = requests.get(api_url, params=params)
 
@@ -50,6 +50,9 @@ def get_survey_data(api_url_base, params):
 			response = requests.get(api_url, params=params)
 			
 			if response.status_code == 200:
+				
+				logger.info('status code 200')
+				
 				raw = response.json()
 				for i in raw['data']:
 					results.append(get_survey_data_row(i))
@@ -57,6 +60,7 @@ def get_survey_data(api_url_base, params):
 			else:
 				print('[!] HTTP {0} calling [{1}]'.format(response.status_code, api_url)) # 401 unauthorized
 
+		logger.info('returning results')
 		return results
 
 	else:
