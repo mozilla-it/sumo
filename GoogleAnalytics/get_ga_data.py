c9648de9c5efb6f433f404dda896911d9fa67f3a
ophie200 <ophie200@users.noreply.github.com>
2019-03-19 11:50:36 -0700

    Update get_ga_data.py

--- GoogleAnalytics/get_ga_data.py
+++ GoogleAnalytics/get_ga_data.py
@@ -1,8 +1,16 @@
 from apiclient.discovery import build
 from oauth2client.service_account import ServiceAccountCredentials
-#import pandas as pd
+import pandas as pd
 import csv
-from datetime import timedelta, date
+from datetime import timedelta, date, timedelta
+from google.cloud import bigquery
+
+bq_client = bigquery.Client()
+dataset_ref = bq_client.dataset('sumo')
+
+from google.cloud import storage
+storage_client = storage.Client()
+
 
 SCOPES = ['https://www.googleapis.com/auth/analytics.readonly']
 KEY_FILE_LOCATION = 'sumo-imposing-union-227917-b3125c4ed9d8.json'
@@ -13,10 +21,9 @@ def daterange(start_date, end_date):
   for n in range(int ((end_date - start_date).days)):
     yield start_date + timedelta(n)
 
+# run historical kb_exit_rate and users_by_country in monthy increments to not hit API limits
 start_date = date(2018, 1, 1)
-#start_date = date(2018,7,16)
-end_date = date(2019, 3, 11) # choked after 20190305
-#end_date = date(2018, 6, 1)
+end_date = date(2019, 3, 12) 
 
 def initialize_analyticsreporting():
   """Initializes an Analytics Reporting API V4 service object.
@@ -319,95 +326,104 @@ def add_response_to_results(response, results):
   return results
 
   
-def run_total_users(analytics):
-  #response = get_total_users(analytics, '2018-01-01', '2019-03-11')
+def run_total_users(analytics, fn):
   response = get_total_users(analytics, start_date.strftime("%Y-%m-%d"), end_date.strftime("%Y-%m-%d"))
-  with open("./ga_data_total_users.csv", "w", encoding='utf8') as f:
-    csv.register_dialect('myDialect', delimiter = ',', quoting=csv.QUOTE_ALL, skipinitialspace=True)
-    writer = csv.writer(f, dialect='myDialect')
-    results = []
-    fields = ["ga_date","ga_users"]
-    results.append(fields)
-    writer.writerows(add_response_to_results(response, results))
-
-
-def run_search_ctr(analytics):
-  response = get_total_users(analytics, start_date.strftime("%Y-%m-%d"), end_date.strftime("%Y-%m-%d"))
-  with open("./ga_data_search_ctr.csv", "w", encoding='utf8') as f:
-    csv.register_dialect('myDialect', delimiter = ',', quoting=csv.QUOTE_ALL, skipinitialspace=True)
-    writer = csv.writer(f, dialect='myDialect')
-    results = []
-    fields = ["ga_date","ga_segment","ga_users","ga_uniqueEvents"]
-    results.append(fields)
-    writer.writerows(add_response_to_results(response, results))
-
-
-def run_inproduct_vs_organic(analytics):
-  response = get_total_users(analytics, start_date.strftime("%Y-%m-%d"), end_date.strftime("%Y-%m-%d"))
-  with open("./ga_data_inproduct_vs_organic.csv", "w", encoding='utf8') as f:
-    csv.register_dialect('myDialect', delimiter = ',', quoting=csv.QUOTE_ALL, skipinitialspace=True)
-    writer = csv.writer(f, dialect='myDialect')
-    results = []
-    fields = ["ga_date","ga_segment","ga_users","ga_sessions"]
-    results.append(fields)
-    writer.writerows(add_response_to_results(response, results))
-
-def run_kb_exit_rate(analytics):
-  #end_date = date(2018, 4, 13) # choked after 20180412; change filenames!!
-  
   results = []
-  
-  with open("./ga_data_top20_kb_exit_rate.csv", "w", encoding='utf8') as f:
-    csv.register_dialect('myDialect', delimiter = ',', quoting=csv.QUOTE_ALL, skipinitialspace=True)
-    writer = csv.writer(f, dialect='myDialect')
-    
-    fields = ["ga_date","ga_exitPagePath","ga_exitRate","ga_exits","ga_pageviews"]
-    results.append(fields)
-    writer.writerows(results)
-    
-    for dt in daterange(start_date, end_date):
-      dt_str = dt.strftime("%Y-%m-%d")
-      print(dt_str)
-      results = []
-      response = get_top20_kb_exit_rate(analytics, dt_str, dt_str)
-      add_response_to_results(response, results)
-      
-      writer.writerows(results)
+  # write csv file to gs sumo folder for processing to BQ
+  df = pd.DataFrame.from_records(add_response_to_results(response, results), columns=['ga_date', 'ga_users']) 
+  df['ga_date'] = pd.to_datetime(df['ga_date'], format="%Y%m%d").dt.strftime("%Y-%m-%d")
+  df.to_csv(fn, index=False)
 
 
-def run_users_by_country(analytics):
-  # ended on 20180111 so start from 1/12; change filenames!!
-
+def run_search_ctr(analytics, fn):
+  response = get_search_ctr_users(analytics, start_date.strftime("%Y-%m-%d"), end_date.strftime("%Y-%m-%d"))
   results = []
+  # write csv file to gs sumo folder for processing to BQ
+  df = pd.DataFrame.from_records(add_response_to_results(response, results), columns=["ga_date","ga_segment","ga_users","ga_uniqueEvents"]) 
+  df['ga_date'] = pd.to_datetime(df['ga_date'], format="%Y%m%d").dt.strftime("%Y-%m-%d")
+  df.to_csv(fn, index=False)
 
-  with open("./ga_data_users_by_country.csv", "w", encoding='utf8') as f:
-    csv.register_dialect('myDialect', delimiter = ',', quoting=csv.QUOTE_ALL, skipinitialspace=True)
-    writer = csv.writer(f, dialect='myDialect')
-    
-    fields = ["ga_date","ga_country","ga_users"]
-    results.append(fields)
-    writer.writerows(results)
-    
-    for dt in daterange(start_date, end_date):
-      dt_str = dt.strftime("%Y-%m-%d")
-      print(dt_str)
-      results = []
-      response = get_users_by_country(analytics, dt_str, dt_str)
-      add_response_to_results(response, results)
-      
-      writer.writerows(results)
+
+def run_inproduct_vs_organic(analytics, fn):
+  response = get_inproduct_vs_organic(analytics, start_date.strftime("%Y-%m-%d"), end_date.strftime("%Y-%m-%d"))
+  results = []
+  # write csv file to gs sumo folder for processing to BQ
+  df = pd.DataFrame.from_records(add_response_to_results(response, results), columns=["ga_date","ga_segment","ga_users","ga_sessions"]) 
+  df['ga_date'] = pd.to_datetime(df['ga_date'], format="%Y%m%d").dt.strftime("%Y-%m-%d")
+  df.to_csv(fn, index=False)
+
+
+def run_kb_exit_rate(analytics, fn):
+
+  df = pd.DataFrame()
+  
+  for dt in daterange(start_date, end_date):
+    dt_str = dt.strftime("%Y-%m-%d")
+    print(dt_str)
+    results = []
+    response = get_top20_kb_exit_rate(analytics, dt_str, dt_str)
+    df = df.append( pd.DataFrame.from_records(add_response_to_results(response, results), columns=["ga_date","ga_exitPagePath","ga_exitRate","ga_exits","ga_pageviews"]) )
+
+  df['ga_date'] = pd.to_datetime(df['ga_date'], format="%Y%m%d").dt.strftime("%Y-%m-%d")
+  df.to_csv(fn, index=False)
+
+
+def run_users_by_country(analytics, fn):
+
+  df = pd.DataFrame()
+  
+  for dt in daterange(start_date, end_date):
+    dt_str = dt.strftime("%Y-%m-%d")
+    print(dt_str)
+    results = []
+    response = get_users_by_country(analytics, dt_str, dt_str)
+    df = df.append( pd.DataFrame.from_records(add_response_to_results(response, results), columns=['ga_date', 'ga_country', 'ga_users']) )
+
+  df['ga_date'] = pd.to_datetime(df['ga_date'], format="%Y%m%d").dt.strftime("%Y-%m-%d")
+  df.to_csv(fn, index=False)
+
+
+def update_bq_table(fn, table_name):
+
+  table_ref = dataset_ref.table(table_name)
+  job_config = bigquery.LoadJobConfig()
+  job_config.source_format = bigquery.SourceFormat.CSV
+  job_config.skip_leading_rows = 1
+  job_config.autodetect = True
+
+  with open(fn, 'rb') as source_file:
+    job = bq_client.load_table_from_file(source_file,table_ref,location='US',job_config=job_config)  # API request
+
+  job.result()  # Waits for table load to complete.
+  # need to add some error check/handling if now all rows loaded
+  print('Loaded {} rows into {}:{}.'.format(job.output_rows, 'sumo', table_name))
 
     
 def main():
   analytics = initialize_analyticsreporting()
-  run_users_by_country(analytics)
-  run_kb_exit_rate(analytics)
-  #response = get_users_by_country(analytics, '2019-02-19', '2019-02-25')
-  #print_response(response)
-  run_inproduct_vs_organic(analytics)
-  run_search_ctr(analytics)
-  run_total_users(analytics)
+  
+  # construct filename from either start/end date vars or from BQ
+  
+  #run_total_users(analytics,"./ga_data_total_users.csv")
+  #update_bq_table("./ga_data_total_users.csv", 'ga_total_users')
+  
+  #fn = "./ga_data_users_by_country_" + start_date.strftime("%Y%m%d") + "_to_" + (end_date - timedelta(days=1)).strftime("%Y%m%d") + ".csv"
+  #run_users_by_country(analytics, fn)
+  #update_bq_table(fn, 'ga_users_by_country')
+  
+  #fn = "./ga_data_inproduct_vs_organic_" + start_date.strftime("%Y%m%d") + "_to_" + (end_date - timedelta(days=1)).strftime("%Y%m%d") + ".csv"
+  #run_inproduct_vs_organic(analytics, fn)
+  #update_bq_table(fn, 'ga_inproduct_vs_organic')
 
+  #fn = "./ga_data_top20_kb_exit_rate_" + start_date.strftime("%Y%m%d") + "_to_" + (end_date - timedelta(days=1)).strftime("%Y%m%d") + ".csv"
+  #run_kb_exit_rate(analytics, fn)
+  #update_bq_table(fn, 'ga_kb_exit_rate')
+
+  fn = "./ga_data_search_ctr_" + start_date.strftime("%Y%m%d") + "_to_" + (end_date - timedelta(days=1)).strftime("%Y%m%d") + ".csv"
+  print("fn: " + fn)
+  #run_search_ctr(analytics, fn)
+  update_bq_table(fn, 'ga_search_ctr')
+  
 
 if __name__ == '__main__':
   main()
