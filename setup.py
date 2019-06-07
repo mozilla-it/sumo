d81ae729be3f291364250d92081d77fbd79f0180
Adam Frank <afrank@mozilla.com>
2019-06-07 07:27:00 -0700

    preparing for some dag changes

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def readme():
 
 
 setup(name='sumo',
-      version='0.1',
+      version='0.2',
       description='Scripts for SUMO data access',
       long_description=readme(),
       keywords='sumo surveygizmo kitsune twitter reviews',
@@ -15,11 +15,13 @@ setup(name='sumo',
       author='Nancy Wong',
       author_email='nawong@mozilla.com',
       license='...',
-      packages=['SurveyGizmo'],
+      packages=['SurveyGizmo','GooglePlaystore','ReleaseCalendar','Kitsune','GoogleAnalytics','Twitter'],
       install_requires=[
           'requests',
+          'grpcio>=1.8.2',
           'google-cloud-logging',
           'google-cloud-storage',
+          'google-cloud-bigquery',
           'google-api-core',
           'SurveyGizmo',
           'fs-gcsfs',
@@ -28,7 +30,10 @@ setup(name='sumo',
       test_suite='nose.collector',
       tests_require=['nose', 'nose-cover3'],
       entry_points={
-          'console_scripts': ['run-surveygizmo=SurveyGizmo.run_get_survey_data:main'],
+          'console_scripts': [
+              'run-surveygizmo=SurveyGizmo.run_get_survey_data:main',
+              'run-kitsune=Kitsune.get_kitsune_data:main',
+          ],
       },
       include_package_data=True,
       zip_safe=False)
