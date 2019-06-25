59a930ae701a687ca6d215e259fbb050ed6c3fee
edin-ogtal <edin@ogtal.dk>
2019-06-25 10:04:03 +0200

    added all columns from original table pr. vus request

--- Product_Insights/Kitsune/create_kitsune_tables.py
+++ Product_Insights/Kitsune/create_kitsune_tables.py
@@ -1,11 +1,29 @@
 # BQ table empty creation and schema defintions for GA tables
 from google.cloud import bigquery
 client = bigquery.Client()
-dataset_ref = client.dataset('analyse_and_tal')
+OUTPUT_DATASET='analyse_and_tal'
+OUTPUT_TABLE='kitsune_sentiment'
 
-def create_kitsune_sentiment():
+def create_kitsune_sentiment(dataset_name, table_name):
   schema = [
       bigquery.SchemaField('question_id', 'INTEGER', mode='NULLABLE'),
+      bigquery.SchemaField('question_content', 'STRING', mode='NULLABLE'),
+      bigquery.SchemaField('created_date', 'TIMESTAMP', mode='NULLABLE'),
+      bigquery.SchemaField('creator_username', 'STRING', mode='NULLABLE'),
+      bigquery.SchemaField('updated', 'TIMESTAMP', mode='NULLABLE'),
+      bigquery.SchemaField('updated_by', 'STRING', mode='NULLABLE'),
+      bigquery.SchemaField('is_solved', 'BOOLEAN', mode='NULLABLE'),
+      bigquery.SchemaField('locale', 'STRING', mode='NULLABLE'),
+      bigquery.SchemaField('product', 'STRING', mode='NULLABLE'),
+      bigquery.SchemaField('title', 'STRING', mode='NULLABLE'),
+      bigquery.SchemaField('topic', 'STRING', mode='NULLABLE'),
+      bigquery.SchemaField('solution', 'INTEGER', mode='NULLABLE'),
+      bigquery.SchemaField('solved_by', 'STRING', mode='NULLABLE'),
+      bigquery.SchemaField('num_votes', 'INTEGER', mode='NULLABLE'),
+      bigquery.SchemaField('num_votes_past_week', 'INTEGER', mode='NULLABLE'),
+      bigquery.SchemaField('last_answer', 'INTEGER', mode='NULLABLE'),
+      bigquery.SchemaField('metadata_array', 'STRING', mode='NULLABLE'),
+      bigquery.SchemaField('tags_array', 'STRING', mode='NULLABLE'),    
       bigquery.SchemaField('score', 'FLOAT', mode='NULLABLE',
                            description='Sentiment score from the google language api'),
       bigquery.SchemaField('magnitude', 'FLOAT', mode='NULLABLE',
@@ -14,17 +32,15 @@ def create_kitsune_sentiment():
                            description='''Contains a simple to understand aggregate score 
                                        of the sentiment such as positive or negative, based 
                                       on the score and magnitude'''),
-      bigquery.SchemaField('topic', 'STRING', mode='NULLABLE',
-                           description='The topic field from the Kitsune api')
   ]
-  table_ref = dataset_ref.table('kitsune_sentiment')
+  dataset_ref = client.dataset(dataset_name)
+  table_ref = dataset_ref.table(table_name)
   table = bigquery.Table(table_ref, schema=schema)
   table = client.create_table(table)
 
 
 def main():
-  create_kitsune_sentiment()
-
+  create_kitsune_sentiment(OUTPUT_DATASET, OUTPUT_TABLE)
 
 if __name__ == '__main__':
   main()
